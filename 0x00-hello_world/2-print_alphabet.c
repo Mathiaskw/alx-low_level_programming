c64f09b7a3f4f17b6c127db0cdcc8b4098562fcd
Mathiaskw <kwikirizamathias616@gmail.com>
2022-07-01 13:28:32 -0700

    testing

---
+++ 0x00-hello_world/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *  return 0 success
+ */
+
+int main(void)
+
+{ char c;
+	c = 'a';
+	intergar_C = putchar(c);
+	while(c <= 'z');
+{
+	c++;
+	return (0);
+}
+}
