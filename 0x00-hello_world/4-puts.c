1dec01c0b5bf07c1b557c2a25379f14e958f6fcd
Mathiaskw <kwikirizamathias616@gmail.com>
2022-07-01 12:13:09 -0700

    Another day

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 /*This is my code
- * am going to try out*/
-int main() {
+ * am going to try out
+ */
+int main(void) 
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
