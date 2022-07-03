781756ff0cd1d169d227dca8b8757dcc33828bd0
Mathiaskw <kwikirizamathias616@gmail.com>
2022-07-03 03:05:20 -0700

    testing

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,12 +1,19 @@
-#include <stdio.h>
+#include	<stdio.h>
 /**
- *main - Entry Point 
- *
- * Return: Always 0 (Success)
- */
+*main - Entry Point 
+*
+*Return: Always 0 (Success)
+*/
 
-int main(void)
-{ printf("with proper grammar, but the outcome is a piece of art,\n");
+int	main(void)
+{	int	ch;
 
+for (ch = 'a'; ch <= 'z'; ch++)
+{	
+	putchar(ch);
+}
+{
+	putchar('\n');
+}
 	return (0);
 }
