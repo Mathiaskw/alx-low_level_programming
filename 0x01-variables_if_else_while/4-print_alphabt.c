12b677f75e3df2664972a6dd2a2b6a73ba745ede
Mathiaskw <kwikirizamathias616@gmail.com>
2022-07-03 03:51:58 -0700

    testing

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,15 @@
+#include	<stdio.h>
+/**
+ *main - main function
+ *Description:printing alphabet in lowercase
+ *Return: 0
+ */
+int	main(void)
+{	int	ch;
+if	(ch != 'q' && ch != 'e')
+{	putchar(ch);
+}
+{	putchar('\n');
+}
+	return (0);
+}
