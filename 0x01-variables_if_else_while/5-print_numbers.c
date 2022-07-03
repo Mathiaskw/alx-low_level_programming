8734b3f6b7b5ec0cdaa9a8630ed0072d6564e2b9
Mathiaskw <kwikirizamathias616@gmail.com>
2022-07-03 04:22:36 -0700

    testing

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,12 +1,12 @@
 #include	<stdio.h>
 /**
- *main - entry point
+ *main - main function
  *
  *Return 0
  */
 int	main(void)
 {	int	a;
-for	(a = 0; a <= 10; a++)
+for	(a = 0; a < 10; a++)
 {	printf("%d", a);
 }
 {	printf("\n");
