a3fc74a726e8412bcb1b8f97eda9064f258c4df1
Mathiaskw <kwikirizamathias616@gmail.com>
2022-07-03 04:45:50 -0700

    testing

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include	<stdio.h>
+/**
+ *main - main function
+ *Return: 0
+ */
+int	main(void)
+{	int	kx;
+for	(kx = 'z'; kx >= 'a'; kx++)
+{	putchar(kx);
+}
+{	putchar('\n');
+}
+	return	(0);
+}
