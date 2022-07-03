4efe261dac3f611b775693f31475f3f144dcd7a0
Mathiaskw <kwikirizamathias616@gmail.com>
2022-07-03 05:15:49 -0700

    testing

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,17 @@
+#include	<stdio.h>
+/**
+ *main - main function
+ *Return: 0
+ */
+int	main(void)
+{	int	i;
+for	(i = '0'; i <= '9'; i++)
+{	putchar((i);
+}
+if	(i < '9')
+{	putchar(',');
+}
+{	putchar(' ');
+}
+	return	(0);
+}
