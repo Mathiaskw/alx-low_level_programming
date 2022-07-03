903ac24744d74000c7763006fa5c676addd369d3
Mathiaskw <kwikirizamathias616@gmail.com>
2022-07-03 05:02:01 -0700

    testing

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,15 @@
+#include	<stdio.h>
+#include	<stdlib.h>
+/**
+ *main - main function
+ *Return: 0
+ */
+int	main(void)
+{	int	jk;
+for	(jk = '0'; jk <= '9'; jk++)
+{	putchar(jk);
+}
+{	putchar('\n');
+}
+	return	(0);
+}
