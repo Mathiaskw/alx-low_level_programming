41c1656348bd9bcd389a1eaefeb8b237f3ec4925
Mathiaskw <kwikirizamathias616@gmail.com>
2022-07-04 04:28:56 -0700

    testing

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,10 +6,14 @@
 int	main(void)
 {	int	i;
 for	(i = '0'; i <= '9'; i++)
-if	(i <= '8' && i == '9')
+if	(i <= '8')
 {	putchar(i);
 	putchar(',');
 	putchar(' ');
+}
+for     (i = '0'; i <= '9'; i++)
+if	(i == '9')
+{	putchar(i);
 }
 {	putchar('\n');
 }
