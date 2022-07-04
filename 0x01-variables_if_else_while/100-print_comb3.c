3ef410014802bdacb4e98240ec4fe63f7c21b0b4
Mathiaskw <kwikirizamathias616@gmail.com>
2022-07-04 03:51:22 -0700

    testing

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,7 +7,7 @@ int	main(void)
 {	int	r,	i;
 for	(r = '0'; r <= '9'; r++)
 for	(i = '0'; i <= '9'; i++)
-if	(r != i && r != i + 1)
+if	(r != i && r != i + 1 && i != r + 1)
 {	putchar(r);
 	putchar(i);
 	putchar(',');
