726f0b68cd8757be1485cbac8a1d31773233d207
Mathiaskw <kwikirizamathias616@gmail.com>
2022-07-05 06:45:56 -0700

    joy

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include	<stdio.h>
+/**
+ *main - main function
+ *Return: Always 0 (Success)
+ */
+int	main(void)
+{	char	name[] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r', '\0'};
+	putchar(name);
+}
+{	putchar('\n');
+}
+	return  (0);
+}
+
