1993c480f4102e0fd12fa9d92befcb2115d8b787
Mathiaskw <kwikirizamathias616@gmail.com>
2022-07-05 07:01:40 -0700

    joys

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,7 +4,7 @@
  *Return: Always 0 (Success)
  */
 int	main(void)
-{	char	n[] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r', '\0'};
+{	char*	n[] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r', '\0'};
 	putchar(n);
 }
 {	putchar('\n');
