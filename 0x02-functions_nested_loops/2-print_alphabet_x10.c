5c5ba87424eccb8366b9eda42fd373721c355df6
Mathiaskw <kwikirizamathias616@gmail.com>
2022-07-06 09:29:51 -0700

    feeling

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,12 @@
+#include	<stdio.h>
+#include	"main.h"
+/**
+ *print_alphabet_x10 - prints letters multiple times
+ *Return: 0
+ */
+void	print_alphabet_x10(void)
+{	char f;
+	_putchar(f);
+}
+	putchar('\n');
+}
