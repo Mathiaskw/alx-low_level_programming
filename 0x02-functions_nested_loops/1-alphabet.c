7d157d59af7e8fb9b220ee8bdb5ff0f0163fe087
Mathiaskw <kwikirizamathias616@gmail.com>
2022-07-06 09:00:09 -0700

    feeling

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include	<stdio.h>
+#include	"main.h"
+/**
+ *print_alphabet - print letter a to z
+ *Return: 0
+ */
+void	print_alphabet(void)
+{	char	k;
+	for	(k = 'a'; k <= 'z'; k++)
+	_putchar(k);
+}
+{	putchar('\n');
+}
+	return	(0);
+}
