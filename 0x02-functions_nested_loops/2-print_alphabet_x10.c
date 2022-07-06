00ff64fa9828001df969caa8d6ffa1faa849ff61
Mathiaskw <kwikirizamathias616@gmail.com>
2022-07-06 10:21:21 -0700

    feeling

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,7 +2,8 @@
 #include	"main.h"
 /**
  *print_alphabet_x10 - prints letters multiple times
- *Return: 0
+ *Description: print_alphabet_x10 - print letters a to z
+ * Return: 0
  */
 void	print_alphabet_x10(void)
 {	char	counter;
