8caf455d5faee88bdf8bd530ce6e8e55ac8440a7
Mathiaskw <kwikirizamathias616@gmail.com>
2022-07-06 08:45:55 -0700

    feeling

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,8 +5,25 @@
  *Return: 0
  */
 int	main(void)
-{	_putchar("_putchar");
-	putchar('\n');
+{	char	k = '_';
+	char	p = 'p';
+	char	m = 'u';
+	char	n = 't';
+	char	a = 'c';
+	char	b = 'h';
+	char	c = 'a';
+	char	d = 'r';
+
+	putchar(k);
+	putchar(p);
+	putchar(m);
+	putchar(n);
+	putchar(a);
+	putchar(b);
+	putchar(c);
+	putchar(d);
+}
+{	putchar('\n');
 }
 	return	(0);
 }
