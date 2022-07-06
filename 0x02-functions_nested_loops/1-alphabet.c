5ac5147e6a32229d3e838d60c6072f74e4745dc8
Mathiaskw <kwikirizamathias616@gmail.com>
2022-07-06 09:46:30 -0700

    feeling

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,11 +5,9 @@
  *Return: 0
  */
 void	print_alphabet(void)
-{	char counter;
-	int	k;
+{	char	k;
 
-for	(k = 0; k < 10; k++)
-{	for	(counter = 'a'; counter <= 'z'; counter++)
+for	(k = 'a'; k <= 'z'; k++)
 {	_putchar(k);
 }
 	putchar('\n');
