b7930984188158aeeea21cf6461abb6de5854bf1
Mathiaskw <kwikirizamathias616@gmail.com>
2022-07-06 10:24:40 -0700

    feeling

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,7 +3,7 @@
 /**
  *print_alphabet_x10 - prints letters multiple times
  *Description: print_alphabet_x10 - print letters a to z
- * Return: 0
+ *Return: 0
  */
 void	print_alphabet_x10(void)
 {	char	counter;
@@ -14,4 +14,5 @@ for	(f = 0; f < 10; f++)
 {	_putchar(counter);
 }
 	_putchar('\n');
+}
 }
