cb16ae1b4ecb6b132c9325f840aa4e044d5fb75f
Mathiaskw <kwikirizamathias616@gmail.com>
2022-07-06 07:59:07 -0700

    joy

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,20 +1,4 @@
 /**
- *This is my own header file
+ *These are my functions
  */
-void	p_utchar(char name[])
-{	printf("%s\n", name);
-}
-void	print_alphabet()
-{	char	c;
-	for	(c = 'a'; c <= 'z'; c++)
-	printf("%c", c);
-}
-void	print_alphabet_x10()
-{	char k;
-	for     (k = 'a'; k <= 'z'; k++)
-	printf("%c\n%c\n%c\n%c\n%c\n%c\n%c\n%c\n%c\n%c\n", k, k, k, k, k, k, k, k, k, k);
-}
-int _islower(int c)
-{	char c = 'y';
-	printf("%c: %d", c, islower(c));
-}
+int	_putchar(char c);
