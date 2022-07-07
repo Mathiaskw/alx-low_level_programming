41fffb517b6c16fcfcbb646589b98a661c6d525d
Mathiaskw <kwikirizamathias616@gmail.com>
2022-07-07 05:25:13 -0700

    done

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include	<stdio.h>
+#include	"main.h"
+/**
+ *print_most_numbers - print numbers 10 times
+ *Return: 0
+ */
+void	print_most_numbers(void)
+{	int i;
+	int r;
+
+for	(i = 0; i < 15; i++)
+{
+for	(r = 0; r < 11; r++)
+{	putchar(r);
+}
+	putchar(i);
+}
+{	putchar('\n');
+}
+}
