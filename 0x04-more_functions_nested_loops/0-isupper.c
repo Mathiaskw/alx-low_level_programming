076a9b98300a55a4e6cc44e67e728a1ca2218235
Mathiaskw <kwikirizamathias616@gmail.com>
2022-07-07 03:44:31 -0700

    done

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include	"main.h"
+#include	<stdio.h>
+/**
+ *_isupper - check the code.
+ *@c:uppercase or lowercase letter
+ *Return: 0
+ */
+int	_isupper(int c);
+{	char c;
+
+	c = 'A';
+	printf("%c: %d\n", c, _isupper(c));
+	return	(1);
+}
+{	c = 'a';
+	printf("%c: %d\n", c, _isupper(c));
+	return	(0);
+}
+}
