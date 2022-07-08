d3dd426c838f0f1aed2cd83da9e2276ba2f46e01
Mathiaskw <kwikirizamathias616@gmail.com>
2022-07-08 10:16:25 -0700

    card

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,10 @@
 #include	"main.h"
 /**
  *main - tests function that prints if integer
- *is positive or negative
+ *
+ * is positive or negative
  *@i - number
+ *
  * Return: 0
  */
 int	main(void)
