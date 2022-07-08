f7e404fab8d3343416a7f7f1904bd2823f723fd6
Mathiaskw <kwikirizamathias616@gmail.com>
2022-07-08 10:26:25 -0700

    card

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -4,13 +4,13 @@
  *Return: 0
  */
 int	main(void)
-{	int i;	
-printf("Infinite loop incoming :(\n");
+{	int i;
+	printf("Infinite loop incoming :(\n");
 	i = 0;
 	/*
-	 * while (i < 10)
+	 *while (i < 10)
 	 *{
-	 *	putchar(i);
+	 *putchar(i);
 	 *}
 	 */
 	printf("Infinite loop avoided! \\o/\n");
