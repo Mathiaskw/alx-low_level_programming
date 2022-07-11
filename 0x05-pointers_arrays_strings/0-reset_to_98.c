61085f08276480f2eba2860d67c7d8933781163d
Mathiaskw <kwikirizamathias616@gmail.com>
2022-07-11 01:04:35 -0700

    t

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -7,11 +7,13 @@
  */
 void	reset_to_98(int *n)
 {	int	n;
-	int	*n	= &n;
+	int	*p;
 
 	n = 402;
+	p = &n;
 	printf("n=%d\n", n);
 	reset_to_98(&n);
+	*p = 98;
 	printf("n=%d\n", n);
 	return (0);
 }
