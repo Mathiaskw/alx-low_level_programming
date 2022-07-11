6da557a814200cf4a046b34ff7899b721c14f954
Mathiaskw <kwikirizamathias616@gmail.com>
2022-07-11 06:53:53 -0700

    t

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,11 @@
+#include	<stdio.h>
+#include	"main.h"
+/**
+ *print_rev - prints string reverse
+ *@str: is string
+ *Return: 0
+ */
+void	print_rev(char *s)
+{	char *s[] = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+}
+
