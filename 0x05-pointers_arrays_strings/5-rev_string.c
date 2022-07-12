ba034a3ef38ed025d3c63da953fc29967d3d59b5
Mathiaskw <kwikirizamathias616@gmail.com>
2022-07-12 02:45:48 -0700

    t

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,12 @@
+#include	<stdio.h>
+#include	"main.h"
+/**
+ *rev_string - reverses a string
+ *@s: is the string to reverse
+ *Return: 0
+ */
+int	main(void)
+{	char a[10] = "My School";
+	*s = &a;
+	rev_string(*s);
+}
