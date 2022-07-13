ab410f1ca84e4f1c4ff938895a96eae26bcd0b99
Mathiaskw <kwikirizamathias616@gmail.com>
2022-07-13 00:10:11 -0700

    tt

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,17 +7,12 @@
  *Return: concanated strings
  */
 char	*_strcat(char *dest, char *src)
-{	char s1[] = "Hello ";
-	char s2[] = "World!\n";
+{	char dest[] = "Hello ";
+	char src[] = "World!\n";
 	char *ptr;
 
-	dest = &s1;
-	src = &s2;
 	ptr = _strcat(dest, src);
-}
-{	putchar(d);
-	putchar('\n');
-}
+	putchar(ptr);
+{	putchar('\n');
 }
 }
-
