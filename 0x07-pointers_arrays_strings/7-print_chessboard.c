cdf8ac18079313d9a584880dccdc11d378e77770
Mathiaskw <kwikirizamathias616@gmail.com>
2022-07-18 08:52:43 -0700

    hello

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,18 @@
+#include	<stdio.h>
+#include	"main.h"
+/**
+ *print_chessboard - print in the terminal the chessboard
+ *@a: The character to print
+ */
+void	print_chessboard(char(*a)[8])
+{	int b;
+	int c;
+
+for	(b = 0; b < 8; b++)
+{
+for	(c = 0; c < 8; c++)
+{	putchar(a[b][c]);
+}
+	putchar('\n');
+}
+}
