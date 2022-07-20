1d1a59fef2ebc2869d8bff36a2ba6c17704b750a
Mathiaskw <kwikirizamathias616@gmail.com>
2022-07-20 02:55:25 -0700

    recurse

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -18,3 +18,12 @@ else
 {	return (square_root(i + 1, j));
 }
 }
+/**
+ *_sqrt_recursion - returns the natural square root of a number
+ *@n: number
+ *Return: the natural square root found
+ */
+int	_sqrt_recursion(int n)
+{	int a = 1;
+	return (square_root(a, n));
+}
