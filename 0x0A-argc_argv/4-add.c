68035c97235eeaef81d56c5f4625b5f1e0b81874
Mathiaskw <kwikirizamathias616@gmail.com>
2022-07-22 03:23:42 -0700

    hardwork

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,26 @@
+#include	<stdio.h>
+#include	<stdlib.h>
+/**
+ *main - add two numbers
+ *@argv: argument vector for values
+ *@argc: argument count
+ *Return: 0
+ */
+int	main(int argc, char *argv[])
+{	int i, j, sum;
+
+	sum = 0;
+for	(i = 1; i < argc ; i++)
+{
+for	(j = 0; argv[i][j] != '\0' ; j++)
+{
+if	(argv[i][j] < 47 || argv[i][j] > 57)
+{	printf("Error\n");
+	return (1);
+}
+}
+	sum = sum + atoi(argv[i]);
+}
+	printf("%d\n", sum);
+	return (0);
+}
