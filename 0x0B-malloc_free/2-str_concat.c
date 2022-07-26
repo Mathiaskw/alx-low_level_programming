abf7f7d3e997d535d4f726c64df1e1c5f185ae96
Mathiaskw <kwikirizamathias616@gmail.com>
2022-07-26 08:17:58 -0700

    malloc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,34 +12,27 @@ char	*str_concat(char *s1, char *s2)
 	char *s;
 
 if	(s1 == NULL)
-{	i = 0;
-}
+	i = 0;
 else
 {
 for	(i = 0; s1[i]; i++)
-{	;
+	;
 }
 if	(s2 == NULL)
-{	j = 0;
-}
-}
+	j = 0;
 else
 {
 for	(j = 0; s2[j]; j++)
-{	;
-}
+	;
 }
 	k = i + j + 1;
 	s = malloc(k * sizeof(char));
 if	(s == NULL)
-{	return (NULL);
-}
+	return (NULL);
 for	(l = 0; l < i; l++)
-{	s[l] = s1[l];
+	s[l] = s1[l];
 for	(l = 0; l < j; l++)
 	s[l + i] = s2[l];
 	s[i + j] = '\0';
-}
-}
 	return (s);
 }
