1ec4b4ba222372fd4826e6db5303c330f51d77f9
Mathiaskw <kwikirizamathias616@gmail.com>
2022-07-26 07:41:20 -0700

    malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,8 +13,8 @@ char	*create_array(unsigned int size, char c)
 
 if	(size == 0)
 {	return (NULL);
-	a = malloc(size * sizeof(char));
 }
+	a = malloc(size * sizeof(char));
 if	(a == NULL)
 {	return (NULL);
 }
