2827902acfc1fb1dfb9552d176355a198ba0e954
Mathiaskw <kwikirizamathias616@gmail.com>
2022-07-26 09:01:40 -0700

    malloc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,37 +2,52 @@
 #include	<stdlib.h>
 #include	"main.h"
 /**
+ *_strlen - length of a string
+ *@s: input char
+ *Return: length of a string
+ */
+int	_strlen(char *s)
+{	int l = 0;
+
+while	(*s != '\0')
+{	s++;
+	l++;
+}
+	return (l);
+}
+/**
  *str_concat - concatenates two strings
  *@s1: first string
  *@s2: second string
  *Return: pointer to newly allocated space in memory, or NULL if error
  */
 char	*str_concat(char *s1, char *s2)
-{	unsigned int i, j, k, l;
-	char *s;
+{	unsigned int l1, l2;
+	char *conc, *tmp;
 
-if	(s1 == NULL)
-	i = 0;
+if	(!s1)
+{	s1 = "";
+}
 else
+{	l1 = _strlen(s1);
+}
+if	(!s2)
+{	s2 = "";
+}
+else
+{	l2 = _strlen(s2);
+}
+	conc = malloc(l1 + l2 + 1);
+if	(!conc)
+{	return (0);
+}
+	tmp = conc;
+while	(*s1)
+{	*tmp++ = *s1++;
+}
+while	((*tmp++ = *s2++))
 {
-for	(i = 0; s1[i]; i++)
 	;
 }
-if	(s2 == NULL)
-	j = 0;
-else
-{
-for	(j = 0; s2[j]; j++)
-	;
-}
-	k = i + j + 1;
-	s = malloc(k * sizeof(char));
-if	(s == NULL)
-	return (NULL);
-for	(l = 0; l < i; l++)
-	s[l] = s1[l];
-for	(l = 0; l < j; l++)
-	s[l + i] = s2[l];
-	s[i + j] = '\0';
-	return (s);
+	return (conc);
 }
